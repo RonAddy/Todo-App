36a6c783f21e82fce8e6f63c1c043ae5ff361158
Aaron Bart-Addison <abartaddison12@gmail.com>
2017-10-25 16:33:49 -0400

    "Constructed first route in todos-routes.js file"

--- routes/todo-routes.js
+++ routes/todo-routes.js
@@ -2,3 +2,9 @@ const todosController = require('../controller/todos-controller');
 const express = require('express');
 
 const todosRouter = express.Router();
+
+
+todosRouter.get('/', todosController.index);
+
+
+module.exports = todosRouter;
